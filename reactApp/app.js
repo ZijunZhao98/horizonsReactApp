f5290cc4185af5ea72fb07adf88252445f494a61
Zijun Zhao <zijunzhao1998@gmail.com>
2017-07-19 13:30:28 -0700

    make list & todo list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+let dummyData = ["wash the dishes", "finish coding homework", "read a book", "do grocery"];
 
-ReactDOM.render(<p>React lives!</p>,
+class ToDo extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <li><button>X</button> {this.props.task}</li>
+    );
+  }
+}
+
+class ToDoList extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    let toDos = dummyData.map((each) => (<ToDo task={each}/>));
+    console.log(toDos);
+    return(
+      <ul>{toDos}</ul>
+    );
+  }
+}
+
+ReactDOM.render(<ToDoList />,
    document.getElementById('root'));
