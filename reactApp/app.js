ddfdeda991f8c2270f52f3308895e369b8a120fe
Zijun Zhao <zijunzhao1998@gmail.com>
2017-07-19 13:45:40 -0700

    add input box

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom'
 
 let dummyData = ["wash the dishes", "finish coding homework", "read a book", "do grocery"];
 
+class InputLine extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="your todo item..." value=""/>
+          <input type="submit" value="Add Todo"/>
+        </form>
+      </div>
+    );
+  }
+}
+
 class ToDo extends React.Component{
   constructor(props){
     super(props)
@@ -27,5 +43,19 @@ class ToDoList extends React.Component{
   }
 }
 
-ReactDOM.render(<ToDoList />,
+class ToDoApp extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <InputLine />
+        <ToDoList />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<ToDoApp />,
    document.getElementById('root'));
