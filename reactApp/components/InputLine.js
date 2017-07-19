714bfe3fbf3f3229520b24380322a1c5641d9de5
Zijun Zhao <zijunzhao1998@gmail.com>
2017-07-19 14:54:37 -0700

    prevented default

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -4,13 +4,25 @@ import ReactDOM from 'react-dom'
 class InputLine extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      typedText: "",
+    }
+  }
+  handleTyping(e){
+    var typed = e.target.value;
+    this.setState({typedText: typed});
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.submit(this.state.typedText);
+    this.setState({typedText: ""});
   }
   render(){
     return (
       <div>
         <form>
-          <input type="text" placeholder="your todo item..." value=""/>
-          <input type="submit" value="Add Todo" onClick = {() => this.props.submit("Test data")}/>
+          <input type="text" placeholder="your todo item..." value={this.state.typedText} onChange = {(e) => {this.handleTyping(e)}}/>
+          <input type="submit" value="Add Todo"  onClick = {(e) => {this.handleSubmit(e)}}/>
         </form>
       </div>
     );
